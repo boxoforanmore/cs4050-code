b74c09ddce06526e3f3fa80f07d23515ffefd3d7
Peter Nielson <computergeekpete@gmail.com>
2018-09-26 09:31:39 -0600

    Added test scripts

---
+++ GraphAssignment/tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo
+echo "-----------------------------------"
+echo "------RUNNING GRAPH UNITTESTS------"
+echo "-----------------------------------"
+echo
+echo "ADJACENCY LIST:"
+echo
+echo "Running: python3 -m unittest ALGraph.py"
+echo `python3 -m unittest ALGraph.py`
+echo
+echo
+echo
+echo "ADJACENCY MATRIX:"
+echo
+echo "Running: python3 -m unittest MatGraph.py"
+echo `python3 -m unittest MatGraph.py`
+echo
+echo
+echo
---
+++ PriorityQueue/tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo
+echo "------------------------------------------"
+echo "-----RUNNING PRIORITY QUEUE UNITTESTS-----"
+echo "------------------------------------------"
+echo
+echo "PRIORITY QUEUE:"
+echo
+echo "Running: python3 -m unittest PriorityQueue.py"
+echo `python3 -m unittest PriorityQueue.py`
+echo
+echo
+echo
