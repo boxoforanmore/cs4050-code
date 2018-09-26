7b7aac390d73eac94dec72a955b4c4c34aea272c
Peter Nielson <computergeekpete@gmail.com>
2018-09-26 09:31:52 -0600

    Added travis shell script

---
+++ travis.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo
+echo
+echo
+`cd ./PriorityQueue/`
+`./PriorityQueue/tests.sh`
+cd ../
+echo
+echo
+wait 1
+echo
+`cd ./GraphAssignment/`
+`./GraphAssignment/tests.sh`
+`cd ../`
+echo
+echo
