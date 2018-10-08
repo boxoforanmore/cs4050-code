d50527991e5ba4e8843b87e06c3c3ec59a059e5b
Peter Nielson <computergeekpete@gmail.com>
2018-10-08 00:05:02 -0600

    Added test script for Sort comparisons

---
+++ SortComparisons/tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo
+echo "-------------------------------------------"
+echo "-----RUNNING SORT COMPARISON UNITTESTS-----"
+echo "-------------------------------------------"
+echo
+echo "SORT COMPARISON:"
+echo
+echo "Running: python3 -m unittest SortComp.py"
+python3 -m unittest SortComp.py
+echo
+echo
+echo
